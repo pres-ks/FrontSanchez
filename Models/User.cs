757c348254d27e870665996eb79fbff56d2ca9a2
Presiga <presigadurangoesteban@gmail.com>
2025-12-02 09:14:54 -0500

    Descripción de los cambios realizados

---
+++ Models/User.cs
@@ -0,0 +1,11 @@
+namespace ConsumoAPI2.Wasm.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+    }
+}
